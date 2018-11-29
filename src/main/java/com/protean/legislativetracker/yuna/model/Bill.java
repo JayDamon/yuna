0ec4340fedb043c93951cd2108ff039bf900af9c
DESKTOP-MUAA09M\JayDamon <jay.n.damon@gmail.com>
2018-11-29 15:29:12 -0500

    Fixed subject wrong reference

--- src/main/java/com/protean/legislativetracker/yuna/model/Bill.java
+++ src/main/java/com/protean/legislativetracker/yuna/model/Bill.java
@@ -70,7 +70,7 @@ public class Bill extends DateAuditable {
     private Set<BillSponsor> sponsors;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "bill", orphanRemoval = true)
     private Set<BillSast> sasts;
-    @ManyToMany(mappedBy = "subjects")
+    @ManyToMany(mappedBy = "bills")
     private Set<Subject> subjects;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "bill")
     private Set<BillText> texts;
