4fc72e231c150265305c982dd7c0f79e61e707b8
DESKTOP-MUAA09M\JayDamon <jay.n.damon@gmail.com>
2018-11-30 07:19:21 -0500

    Changed cascade type for bill sponsor

--- src/main/java/com/protean/legislativetracker/yuna/model/BillSponsor.java
+++ src/main/java/com/protean/legislativetracker/yuna/model/BillSponsor.java
@@ -12,7 +12,7 @@ public class BillSponsor implements MappedBillId<BillSponsorId> {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "bill_id", insertable = false, updatable = false)
     private Bill bill;
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "person_id")
     private Person person;
     @OneToOne(fetch = FetchType.LAZY)
